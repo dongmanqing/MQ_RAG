9690c42140a342f802a0befa9ad2b8eebf22c77e
Manqing Dong <MQ10006376@Manqings-MacBook-Air.local>
2024-06-20 16:31:16 +1000

    changes

--- embedding_model.py
+++ embedding_model.py
@@ -17,7 +17,7 @@ class EmbeddingModel:
         return self.embedding_model
 
     def public_embed(self, **kwargs):
-        model_kwargs = kwargs.get("model_kwargs", {'device': 'cpu'})
+        model_kwargs = kwargs.get("model_kwargs", {'device': 'cuda'})
         encode_kwargs = kwargs.get("encode_kwargs", {'normalize_embeddings': False})
         embeddings_model = HuggingFaceEmbeddings(
             model_name=self.public_model,
